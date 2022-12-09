14445ca6ed356b1b44bab47003af52c61eba2139
amanvyas12 <avyas1510march@gmail.com>
2022-12-09 23:53:32 +0530

    pwa

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import '../public/worker' 
-// import * as serviceWorker from './serviceWorker';
 import * as serivceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,6 +11,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
-reportWebVitals();
-// serviceWorker.register();
 serivceWorkerRegistration.register();
+reportWebVitals();
