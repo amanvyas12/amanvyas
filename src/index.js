8852364067e1bc81370ed2558f940645dccd26a2
amanvyas12 <avyas1510march@gmail.com>
2022-12-09 23:44:55 +0530

    pwa updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import '../public/worker' 
+// import * as serviceWorker from './serviceWorker';
+import * as serivceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,3 +14,5 @@ root.render(
   </React.StrictMode>
 );
 reportWebVitals();
+// serviceWorker.register();
+serivceWorkerRegistration.register();
