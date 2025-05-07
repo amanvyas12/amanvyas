bcf4606f315129463763a23639ffa8a86c768737
amanvyas12 <avyas1510march@gmail.com>
2025-05-07 12:28:17 +0530

    updated testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,6 +3,7 @@ import './testimonials.css';
 import AVTR1 from '../../assests/avatar11.jpg';
 import AVTR2 from '../../assests/nayabb.png';
 import AVTR3 from '../../assests/MTI.png';
+import AVTR4 from '../../assests/reflexion_medical_logo.jpeg'
 
 // import Swiper core and required modules
 import {  Pagination, Navigation } from 'swiper';
@@ -16,6 +17,11 @@ import 'swiper/css/pagination';
 
 const Testimonials = () => {
   const data = [
+    {
+      avatar: AVTR4,
+      name: 'RefleXion',
+      review: 'Working with Aman on the SCINTIX Treatment Planning and Delivery System at RefleXion Medical was a game-changer for our engineering team. His deep expertise with Angular and Electron transformed what was once a slow, error-prone process into a streamlined, high-efficiency solution. Aman not only cut plan generation time by 20%, but also helped us significantly improve treatment accuracy and patient safety — all while making the system more reliable and responsive in real time. His ability to blend strong technical skills with thoughtful problem-solving is truly exceptional. The automation tool he developed using React and Playwright became a vital part of our QA workflow, allowing us to catch issues early and move faster. Aman is not just a developer — he is an architect of better outcomes.'
+    },
     {
       avatar: AVTR1,
       name: 'Reliance General Insurance',
